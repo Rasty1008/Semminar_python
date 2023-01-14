e639da51e508f67d1b296e0a2965c2c9c06c2d83
Rostislav <teterukrostislav@gmil.com>
2023-01-15 00:08:32 +0600

    add new decision

--- lesson_five/task5.py
+++ lesson_five/task5.py
@@ -29,3 +29,28 @@ if lst[-1] > L[ -1]:
 if lst[0] < L[0]:
     L.insert(0, lst[0])
 print(L)
+
+#Вариант решение рекурсией
+from pprint import pprint
+
+numbers = [1, 5, 2, 3, 4, 6, 1, 2, 7]
+seq = set()
+
+def find_sequenses(arr, cur_i = 0, cur_seq = tuple()):
+    #Точка выхода: когда индекс вышел за границу
+    if cur_i == len(arr):
+        if len(cur_seq) > 1:
+            seq.add(cur_seq)
+        return
+
+    #Ищем дальше, игнурируя текущий элемент
+    find_sequenses(arr, cur_i + 1, cur_seq)
+
+    #Ищем дальше, используя текущий элемент, но только если он подходящий
+    if (cur_seq and cur_seq[-1] <= arr[cur_i] or not(cur_seq)):
+        find_sequenses(arr, cur_i + 1, cur_seq + (arr[cur_i],))
+
+find_sequenses(numbers)
+
+pprint(seq)
+print(len(seq))
